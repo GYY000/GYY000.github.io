cbd43f2115d5fd9c2a71d528caf28548efa2e1ed
GhostOnSill <1037967826@qq.com>
2022-12-30 15:10:43 +0800

    Site updated: 2022-12-30 15:10:43

--- js/cover.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  let $banner = $('#cover #banner');
-  let bannerUrls = $banner.attr('data-banners').split(';');
-  let randomBannerUrl = bannerUrls[Math.floor(Math.random() * bannerUrls.length)];
-
-  $banner.css('background-image', 'url(' + randomBannerUrl + ')');
-})(jQuery);
